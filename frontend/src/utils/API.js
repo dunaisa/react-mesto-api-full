51b26a69450d06a1f2775a5527111cb2f8a4968d
Elizabeth <dun1isa@yandex.ru>
2022-10-24 22:27:47 +0300

    test

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -95,7 +95,7 @@ class Api {
 }
 
 export const api = new Api({
-  url: 'http://localhost:5555',
+  url: 'https://api.memesto.nomoredomains.icu',
   headers: {
     authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
     'Content-Type': 'application/json'
