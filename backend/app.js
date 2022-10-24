d2c6a02c488e7a480329b153078554bfabdd45b3
Elizabeth <dun1isa@yandex.ru>
2022-10-24 23:27:16 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const path = require('path');
 
 const { auth } = require('./middlewares/auth');
 
@@ -27,14 +27,15 @@ const allowedCors = [
   'http://127.0.0.1'
 ];
 
-app.use(express.static('build'));
+// app.use(express.static('build'));
 
-// Serve the index.html for all the other requests so that the
-// router in the JavaScript application can render the necessary components
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+// // Serve the index.html for all the other requests so that the
+// // router in the JavaScript application can render the necessary components
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/build/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(requestLogger);
 
