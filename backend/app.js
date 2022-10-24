29035afc02a901488cd3f3a223959e1e34c07247
Elizabeth <dun1isa@yandex.ru>
2022-10-24 23:05:12 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -27,7 +26,14 @@ const allowedCors = [
   'http://127.0.0.1'
 ];
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
+
+// Serve the index.html for all the other requests so that the
+// router in the JavaScript application can render the necessary components
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 app.use(requestLogger);
 
