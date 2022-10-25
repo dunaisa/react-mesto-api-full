003a2460021febce8f0cc8248f9f66ae7c1d4bf6
Elizabeth <dun1isa@yandex.ru>
2022-10-25 11:16:11 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,29 @@ const allowedCors = [
   'http://127.0.0.1'
 ];
 
+app.use(function (req, res, next) {
+  const { method } = req;
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
+
 // app.use(express.static('build'));
 
 // // Serve the index.html for all the other requests so that the
@@ -59,28 +82,7 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use(function (req, res, next) {
-  const { method } = req;
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers['access-control-request-headers'];
 
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  next();
-});
 
 app.use('/*', (req, res, next) => {
   next(new ObjectNotFound('Запрашиваемый путь не существует.'));
