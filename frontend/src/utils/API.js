d4059ee1a80e07461e167be9cec17fdc20abf56f
Elizabeth <dun1isa@yandex.ru>
2022-10-27 20:13:19 +0300

    test

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -121,7 +121,7 @@ class Api {
 }
 
 export const api = new Api({
-  url: 'http://api.memesto.nomoredomains.icu',
+  url: 'https://api.memesto.nomoredomains.icu',
   headers: {
     // authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
     authorization: `Bearer ${localStorage.getItem('token')}`,
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.memesto.nomoredomains.icu';
+export const BASE_URL = 'https://api.memesto.nomoredomains.icu';
 
 const checkResponse = (res) => {
   if (res.ok) {
